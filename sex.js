23d424618e3734ee9c421128aa666d97e792dd7e
jdj0711 <106151933+jdj0711@users.noreply.github.com>
2022-05-24 19:14:39 +0900

    Create sex.js

---
+++ sex.js
@@ -0,0 +1,9 @@
+var canvas = document.querySelector('canvas');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var c = canvas.getContext('2d');
+
+c.fillStyle = 'rgba(255, 0, 0, 0, 5)';
+c.fillRect(100, 100, 100, 100);
